308e881f77edf2fe083a284d9905de18377a5f61
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-03-06 17:16:30 +0900

    General layout for dashboard created

--- project/src/app/api/hello/route.ts
+++ project/src/app/api/hello/route.ts
@@ -1,3 +1,29 @@
 export async function GET(request: Request) {
   return new Response('Hello, Next.js!')
 }
+
+interface Product {
+    id: Number
+    name: string
+    price: Number
+    image: string
+    active: boolean
+    seller: string
+    brand: string
+    model: string
+    color: string
+    category: string
+    description: string
+}
+
+// export async function getProducts() {
+//   const data = await fetch('http://localhost:3000/data.json')
+//   const products = await data.json()
+//   return (
+//     <ul>
+//     {products.map((product) => (
+//       <li key={product.id}>{product.name}</li>
+//     ))}
+//     </ul>
+//   )
+// }
--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -6,86 +6,43 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='px-8 py-2 '>
+      <nav className='border-b-gray-500 border-b-2 flex justify-between py-3 '>
+
+        {/* Nav links and logo */}
+        <div className=' flex gap-5 items-center'>
+          {/* <Image src={"/logo.png"} alt='Logo' width={50} height={50}/> */}
+          <h1 className=' py-3 '>Logo</h1>
+          <ul className='flex gap-5'>
+            <li className='bg-slate-800 rounded-xl p-3'>Dashboard</li>
+            <li className='p-3'>Browse</li>
+            <li className='p-3'>Cart</li>
+          </ul>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        {/* Profile */}
+        <div className='flex gap-5 text-3xl'>
+          <button>🔔</button>
+          <button>🙂</button>
         </div>
+      </nav>
+
+    
+      <h2 className='font-bold text-4xl my-10'>Dashboard</h2>
+      <div className='bg-slate-300 text-black flex gap-5 flex-col p-5 rounded'>
+        {/* Top Bar */}
+        <div className='flex justify-between '>
+          <div>
+            <h3 className='text-xl font-bold'>Favorites</h3>
+            <p className='opacity-80'>A list of your favorites items to keep track of.</p>
+          </div>
+          <button className='bg-cyan-700 px-5 text-white rounded'>+ Add</button>
+        </div>
+
+        {/* Table */}
+        <p>Placeholder</p>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      
     </main>
   )
 }
